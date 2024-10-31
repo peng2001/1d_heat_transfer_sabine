25ffcdde969a21539af207950157d8eda3e37ee9
peng2001 <yuangpeng2001@gmail.com>
2024-10-31 16:37:46 +0000

    test

--- run_steady_state.py
+++ run_steady_state.py
@@ -9,30 +9,38 @@ def config_import():
         inputs = toml.load(f)
     return inputs
 
-def graphing_3d(temperatures_list, time_record, cells_list):
+def graphing_steady_state(temperatures_list, time_record, cells_list):
     x = np.array(cells_list)
     t = np.array(time_record)
     temperature_data = np.array(temperatures_list)
     x, t = np.meshgrid(x, t)
     # Flatten the data for plotting
     # Plotting the data
-    fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     ax.plot(x[-1], temperature_data[-1])
     # Adding axis labels
-    ax.set_xlabel('x (m)')
-    ax.set_ylabel('Temperature (Degrees C)')
+    ax.set_xlabel('x (mm)')
+    ax.set_ylabel('Temperature (Degres C)')
     ax.set_title('1D Temperature')
     plt.show()
 
-
+def calculate_dTdt(temperatures_list, dt):
+    # calculates dT/dt at centre of model to make sure that steady state is reached
+    mid_index = len(temperatures_list[0]) // 2
+    dTdt = (temperatures_list[-1][mid_index] - temperatures_list[-2][mid_index])/dt
+    return dTdt
+        
 if __name__ == "__main__":
     inputs = config_import()
-    time_record, cells_list, cells_temperatures_init = model.model_setup(dx = inputs["dx"], dt = 5, total_time = 100000, temperature_initial = inputs["Temperature_initial"]+273.15, thickness = inputs["Thickness"])
+    dx_precise = np.float64(inputs["dx"])
+    time_record, cells_list, cells_temperatures_init = model.model_setup(dx = dx_precise, dt = inputs["dt"], total_time = inputs["total_time"], temperature_initial = inputs["Temperature_initial"]+273.15, thickness = inputs["Thickness"])
     temperatures = model.run_model(time_record=time_record, cells_list=cells_list, cells_temperatures_init=cells_temperatures_init,
                                     Temperature_side_minusL=inputs["Temperature_side_minusL"]+273.15, Temperature_side_plusL=inputs["Temperature_side_plusL"]+273.15, 
-                                    dt=inputs["dt"], dx=inputs["dx"], Heat_gen=inputs["Heat_gen"], k=inputs["Conductivity"], rho=inputs["Density"], cp=inputs["Specific_heat"])
+                                    dt=inputs["dt"], dx=dx_precise, Heat_flux=inputs["Heat_flux"], k=inputs["Conductivity"], rho=inputs["Density"], cp=inputs["Specific_heat"])
     temperatures_C = [[value - 273.15 for value in sublist] for sublist in temperatures] # subtract 273.15 from all points to convert to deg C
     print("Avg final temperature: "+str(round(np.mean(temperatures_C[-1]), 4))+" Degrees C")
     print("Max final temperature: "+str(round(max(temperatures_C[-1]), 4))+" Degrees C")
     print("Min final temperature: "+str(round(min(temperatures_C[-1]), 4))+" Degrees C")
-    graphing_3d(temperatures_C, time_record, cells_list)
+    print("Final centre dT/dt: "+str(calculate_dTdt(temperatures_list=temperatures_C, dt=inputs["dt"]))) # should be very small; otherwise increase simulation time
+    graphing_steady_state(temperatures_C, time_record, cells_list)
